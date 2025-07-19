bc79d7e64ab81dd58bb5bfb33686a46506229dcd
Igor Medeiros <igor.medeiros@gmail.com>
2025-07-19 14:52:14 -0300

    removidos arquivos desnecessários

--- langchain-na-pratica/capitulo_04/execucao_exercicio_4_0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python capitulo_04/chain_sequencial_antiga.py
--- langchain-na-pratica/exercicios_revisados/run_exercicio_1.bat
+++
@@ -1,8 +0,0 @@
-REM Navigate to the script's directory
-cd /d "%~dp0"
-
-REM Activate the uv virtual environment
-call ..\.venv\Scripts\activate
-
-REM Execute the Python script
-python exercicio_1.py
